81154e6c50cf12d16a76c227386735189075d225
RaunaqSuri92 <raunaqsuri14@gmail.com>
2019-09-25 14:58:40 +0530

    Update PhabFile.c

--- PhabFile.c
+++ PhabFile.c
@@ -3,6 +3,6 @@ int main(){
 	int a = 10;
 	int b = 20;
 	int c = a+b;
-	printf("git and jenkins together performs this operation and gives 40 aha %d!\n",c);
+	printf("git and jenkins together performs this operation and gives 40 ahaa %d!\n",c);
 	return 0;
 }
